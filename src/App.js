17ddc2812e439b5c4aa5818b3d2a2f85b667efff
Rufi-git <rufialiyev2005@gmail.com>
2022-11-11 14:59:55 -0800

    hasrouyter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouteras as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
 import React from "react";
 import { useEffect, useState } from "react";
